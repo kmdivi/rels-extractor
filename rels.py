a00f17b96a6f20ace5b2ca679dc43e86191021aa
kmdivi <kmdivix@gmail.com>
2019-06-10 00:19:56 -0700

    first commit

---
+++ rels.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as et
+import sys
+import zipfile
+
+
+def rels_extract(f):
+    d = zipfile.ZipFile(f)
+    try:
+        xml = d.read('word/_rels/settings.xml.rels')
+        d.close()
+        root = et.fromstring(xml)
+        for i in root:
+            if 'Target' in i.attrib:
+                return i.attrib["Target"]
+            return 'URL Not found'
+    except:
+        return 'word/_rels/settings.xml.rels Not found'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage:\n\tpython3 rels.py [file.docx]')
+        exit(0)
+
+    file = sys.argv[1]
+    print(rels_extract(file))
