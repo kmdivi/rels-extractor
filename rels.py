de6be0752f50cf6d69d62278b474c49164b15db8
kmdivi <kmdivix@gmail.com>
2021-05-19 18:46:40 +0900

    code formatting

--- rels.py
+++ rels.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import xml.etree.ElementTree as et
 import sys
+import xml.etree.ElementTree as et
 import zipfile
 
 
 def rels_extract(f):
     d = zipfile.ZipFile(f)
     try:
-        xml = d.read('word/_rels/settings.xml.rels')
+        xml = d.read("word/_rels/settings.xml.rels")
         d.close()
         root = et.fromstring(xml)
         for i in root:
-            if 'Target' in i.attrib:
+            if "Target" in i.attrib:
                 return i.attrib["Target"]
-            return 'URL Not found'
+            return "URL Not found"
     except:
-        return 'word/_rels/settings.xml.rels Not found'
+        return "word/_rels/settings.xml.rels Not found"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('usage:\n\tpython3 rels.py [file.docx]')
+        print("usage:\n\tpython3 rels.py [file.docx]")
         exit(0)
 
     file = sys.argv[1]
